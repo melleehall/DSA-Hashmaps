5bd7845a0d351d7148733f6eb57c811c345f94c1
Melanie Hallowell <melleehall@gmail.com>
2020-05-11 11:54:31 -0600

    Added _resize, delete, getmethods to Hashmap component and revised other methods to reflect DELETE marker for deleted keys

--- Hashmap.js
+++ Hashmap.js
@@ -6,22 +6,24 @@ class HashMap {
         this._deleted = 0;
     }
 
-    // famous djb2 algorithm - takes a string and hashes it
-    static _hashString(string) {
-        let hash = 5381;
-        for (let i = 0; i < string.length; i++) {
-            hash = (hash << 5) + hash + string.charCodeAt(i);
-            hash = hash & hash;
+    get(key) {
+        const index = this._findSlot(key);
+        if (this._hashTable[index] === undefined) {
+            throw new Error('Key error');
         }
-        return hash >>> 0;
+        return this._hashTable[index].value;
     }
 
+    // Best & average-case performance is O(1) and worse case is O(n)
+    // if collision takes place.
     set(key, value) {
+        // First check if load ratio is creater than given maximum
+        // and if so, resize.
         const loadRatio = (this.length + this._deleted + 1) / this._capacity;
         if (loadRatio > HashMap.MAX_LOAD_RATIO) {
             this._resize(this._capacity * HashMap.SIZE_RATIO);
         }
-        // Find the slot where this should be in
+        // Find the slot where this key should be
         const index = this._findSlot(key);
 
         if(!this._hashTable[index]) {
@@ -34,8 +36,27 @@ class HashMap {
         }
     }
 
+    // Instead of actually deleting an item, just put a deleted marker
+    // in the slot to avoid confusion with items that are located in 
+    // a different slot to collision
+    // Clear out all deleted items when you resize though
+    delete(key) {
+        const index = this._findSlot(key);
+        const slot = this._hashTable[index];
+        if (slot === undefined) {
+            throw new Error('Key error');
+        }
+        slot.DELETED = true;
+        this.length--;
+        this._deleted++;
+    }
+
     // _hashTable array will never be full due to our max load factor 
-    // so the function will always return a slot
+    // so the function will always return a slot.
+    // Best & average-case performance is O(1) assuming the hash 
+    // function is good and load ratio is suitable minimizing chances
+    // of collision and keeping need to iterate low.
+    // Worse case is O(n) since you have to linearly search each slot.
     _findSlot(key) {
         // finds the correct slot for a given key using the private 
         // hashString() function to calculate the hash of the key
@@ -52,6 +73,36 @@ class HashMap {
                 return index;
             }
         }
+    }
+
+    // Not really resizing - actually recreating the has map from
+    // scratch with larger capacity.
+    // Best and average case are O(n) since each set call is O(1)
+    // in the best and average case
+    // Worst case is O(n^n) since each set call is O(n) in the worst case
+    _resize(size) {
+        const oldSlots = this._hashTable;
+        this._capacity = size;
+        // Reset the length bc it will get rebuilt as you add items back
+        this._length = 0;
+        this._deleted = 0;
+        this._hashTable = [];
+
+        for (const slot of oldSlots) {
+            if (slot !== undefined && !slot.DELETED) {
+                this.set(slot.key, slot.value);
+            }
+        }
+    }
+
+    // famous djb2 algorithm - takes a string and hashes it
+    static _hashString(string) {
+        let hash = 5381;
+        for (let i = 0; i < string.length; i++) {
+            hash = (hash << 5) + hash + string.charCodeAt(i);
+            hash = hash & hash;
+        }
+        return hash >>> 0;
     }
 
 }
